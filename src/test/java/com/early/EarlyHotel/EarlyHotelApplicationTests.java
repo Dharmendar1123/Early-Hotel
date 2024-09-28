54e1876740ab6fba10e5d7b9c3290c44d634b99d
dharmendar <dharmendar1123@gmail.com>
2024-09-28 19:03:45 +0530

    create user booking room entity

---
+++ src/test/java/com/early/EarlyHotel/EarlyHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.early.EarlyHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarlyHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
