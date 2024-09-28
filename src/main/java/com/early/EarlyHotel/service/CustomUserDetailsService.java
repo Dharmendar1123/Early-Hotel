ebcf1c94c9915c31ea7bdc51251ad2061943ab6a
dharmendar <dharmendar1123@gmail.com>
2024-09-28 19:32:22 +0530

    create user details service

---
+++ src/main/java/com/early/EarlyHotel/service/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.early.EarlyHotel.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.early.EarlyHotel.exception.OurException;
+import com.early.EarlyHotel.repo.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(() -> new OurException("Username/Email not Found"));
+    }
+}
